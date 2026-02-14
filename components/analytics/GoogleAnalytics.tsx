437272322155203c745cc57543b81c277692d950
Purohit Lokesh Shivkumar <purohitlokesh46@gmail.com>
2026-02-14 20:49:38 +0530

    updated the google analytics page

--- components/analytics/GoogleAnalytics.tsx
+++ components/analytics/GoogleAnalytics.tsx
@@ -2,50 +2,36 @@
 
 import Script from "next/script";
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 export default function GoogleAnalytics({ ga_id }: { ga_id: string }) {
-	useEffect(() => {
-		async function trackVisitor() {
-			try {
-				// Get IP and location from a public API
-				const res = await fetch("https://ipapi.co/json/");
-				const data = await res.json();
-				const ipAddress = data.ip;
-				const location = `${data.city}, ${data.region}, ${data.country_name}`;
-				await fetch("/api/analytics", {
-					method: "POST",
-					headers: { "Content-Type": "application/json" },
-					body: JSON.stringify({ ipAddress, location }),
-				});
-			} catch {
-				// Ignore errors
-			}
-		}
-		trackVisitor();
-	}, []);
+  const pathname = usePathname();
 
-	return (
-		<>
-			<Script
-				strategy='afterInteractive'
-				src={`https://www.googletagmanager.com/gtag/js?id=${ga_id}`}
-			/>
+  useEffect(() => {
+    if (!ga_id || typeof window.gtag !== "function") return;
 
-			<Script
-				id='google-analytics'
-				strategy='afterInteractive'
-				dangerouslySetInnerHTML={{
-					__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${ga_id}', {
-              page_path: window.location.pathname,
-              cookie_flags: 'SameSite=None;Secure',
-            });
-          `,
-				}}
-			/>
-		</>
-	);
+    window.gtag("config", ga_id, {
+      page_path: pathname,
+      page_title: document.title,
+    });
+  }, [pathname, ga_id]);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${ga_id}`}
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          window.gtag = gtag;
+          gtag('js', new Date());
+          gtag('config', '${ga_id}');
+        `}
+      </Script>
+    </>
+  );
 }
